d608a0065fbd8deb3149eaaba407e3d1c5a2c52c
faresarar <faresarar455@gmail.com>
2024-07-16 11:19:24 +0100

    Adding Number Category

--- lib/screens/number_screen.dart
+++ lib/screens/number_screen.dart
@@ -10,6 +10,42 @@ class NumberScreen extends StatefulWidget {
 class _NumberScreenState extends State<NumberScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff46322B),
+        title: const Text("Numbers"),
+      ),
+      body: Container(
+        color: const Color(0xffEF9235),
+        child: Row(
+          children: [
+            Container( color :  const Color(0xffFFF6DC),child: Image.asset("assets/images/numbers/number_one.png")),
+             const Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "ichi",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  Text(
+                    "one ",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+
+                ],
+
+              ),
+            ),
+             const Spacer(flex: 1,),
+             const Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.play_arrow , color: Colors.white,size: 24,),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
