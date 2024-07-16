be6abaa3976e7d9db2ad3be07b51f7b980611cda
faresarar <faresarar455@gmail.com>
2024-07-16 09:56:34 +0100

    Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,79 @@ class TokuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Toku App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Scaffold(
+      home: Scaffold(
+        backgroundColor: Color(0xffFEF6DB),
         appBar: AppBar(
-          title:  Text("TokuApp"),
+          backgroundColor: Color(0xff46322B),
+          title: Text("TokuApp"),
         ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity, // take the space available to you
+              color: Color(0xffEF9235),
+              child: Text(
+                "Members",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity, // take the space available to you
+              color: Color(0xff558B37),
+              child: Text(
+                "FamilyMembers",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity, // take the space available to you
+              color: Color(0xff79359F),
+              child: Text(
+                "Colors",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 24),
+              alignment: Alignment.centerLeft,
+              height: 65,
+              width: double.infinity, // take the space available to you
+              color: Color(0xff50ADC7),
+              child: Text(
+                "Phareses",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
 
+          ],
+        ),
       ),
     );
   }
